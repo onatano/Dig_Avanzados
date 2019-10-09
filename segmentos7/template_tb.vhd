61533774efab93162809452894e395a461440696
onatano <46735222+onatano@users.noreply.github.com>
2019-10-09 11:35:39 -0500

    09-10

--- segmentos7/template_tb.vhd
+++ segmentos7/template_tb.vhd
@@ -3,21 +3,28 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 USE IEEE.MATH_REAL.ALL;
 
-ENTITY template_tb IS
+ENTITY segmentos7_tb IS
 END ENTITY;
 
-ARCHITECTURE tb OF template_tb IS
+ARCHITECTURE tb OF segmentos7_tb IS
 
 --- components DUTs
 
+component segmentos7 is
+	port (a: in std_logic_vector (3 downto 0); --4 entradas
+		s: out std_logic_vector (6 downto 0) -- 7 salidas
+		);
+end component;
 
 --- constants
 	constant 	clk_T			: 	time   :=20 ns;
 
 ---signals
 	
-	signal clock 	: std_logic;
-	signal stop_clk	: boolean;
+	signal clk 	: std_logic;
+  signal stop_clk	: boolean;
+  signal A : std_logic_vector(3 downto 0);
+  signal S : std_logic_vector(6 downto 0);
 
 begin
 
@@ -30,7 +37,7 @@ stimulus: process
     
   stop_clk  <= false;
   -- Put initialisation code here
-
+  
 
 	stop_clk <= true;
 
